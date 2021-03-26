8e530e5559ee681f7eac655f1c60b60a97e9856a
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2021-03-26 11:45:39 -0500

    bring in landing page header

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,14 +1,16 @@
 import React from "react";
 
 // reactstrap components
-// import {
-//
-// } from "reactstrap";
+import {
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
 
 // Core Components
-import ScrollTransparentNavbar from "../components/navbar/ScrollTransparentNavbar.js";
+import ScrollTransparentNavbar from "../components/Navbars/ScrollTransparentNavbar.js";
 
-import HeaderPresentation from "../components/header/HeaderPresentation.js";
+import LandingPageHeader from "../components/Headers/LandingPageHeader.js";
 
 
 function LandingPage() {
@@ -23,7 +25,94 @@ function LandingPage() {
   return (
     <>
       <ScrollTransparentNavbar />
-      <HeaderPresentation />
+      <LandingPageHeader />
+      <div className="section section-about-us">
+          <Container>
+            <Row>
+              <Col className="ml-auto mr-auto text-center" md="8">
+                <h2 className="title">Who we are?</h2>
+                <h5 className="description">
+                  According to the National Oceanic and Atmospheric
+                  Administration, Ted, Scambos, NSIDClead scentist, puts the
+                  potentially record low maximum sea ice extent tihs year down
+                  to low ice extent in the Pacific and a late drop in ice extent
+                  in the Barents Sea.
+                </h5>
+              </Col>
+            </Row>
+            <div className="separator separator-info"></div>
+            <div className="section-story-overview">
+              <Row>
+                <Col md="6">
+                  <div
+                    className="image-container image-left"
+                    style={{
+                      backgroundImage:
+                        "url(" + require("assets/img/stock/stockCateringImage.jpeg") + ")",
+                    }}
+                  >
+                    <p className="blockquote blockquote-info">
+                      "Over the span of the satellite record, Arctic sea ice has
+                      been declining significantly, while sea ice in the
+                      Antarctichas increased very slightly" <br></br>
+                      <br></br>
+                      <small>-NOAA</small>
+                    </p>
+                  </div>
+                  <div
+                    className="image-container image-left-bottom"
+                    style={{
+                      backgroundImage:
+                        "url(" + require("assets/img/stock/stockCateringImage.jpeg") + ")",
+                    }}
+                  ></div>
+                </Col>
+                <Col md="5">
+                  <div
+                    className="image-container image-right"
+                    style={{
+                      backgroundImage:
+                        "url(" + require("assets/img/stock/stockCateringImage.jpeg") + ")",
+                    }}
+                  ></div>
+                  <h3>
+                    So what does the new record for the lowest level of winter
+                    ice actually mean
+                  </h3>
+                  <p>
+                    The Arctic Ocean freezes every winter and much of the
+                    sea-ice then thaws every summer, and that process will
+                    continue whatever happens with climate change. Even if the
+                    Arctic continues to be one of the fastest-warming regions of
+                    the world, it will always be plunged into bitterly cold
+                    polar dark every winter. And year-by-year, for all kinds of
+                    natural reasons, there’s huge variety of the state of the
+                    ice.
+                  </p>
+                  <p>
+                    For a start, it does not automatically follow that a record
+                    amount of ice will melt this summer. More important for
+                    determining the size of the annual thaw is the state of the
+                    weather as the midnight sun approaches and temperatures
+                    rise. But over the more than 30 years of satellite records,
+                    scientists have observed a clear pattern of decline,
+                    decade-by-decade.
+                  </p>
+                  <p>
+                    The Arctic Ocean freezes every winter and much of the
+                    sea-ice then thaws every summer, and that process will
+                    continue whatever happens with climate change. Even if the
+                    Arctic continues to be one of the fastest-warming regions of
+                    the world, it will always be plunged into bitterly cold
+                    polar dark every winter. And year-by-year, for all kinds of
+                    natural reasons, there’s huge variety of the state of the
+                    ice.
+                  </p>
+                </Col>
+              </Row>
+            </div>
+          </Container>
+        </div>
       <div className="wrapper" type="primary">
         <p>Hello</p>
       </div>
