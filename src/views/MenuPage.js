c4db5c0782c26a557c7f4d9d2297f9fb39f39e1c
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2021-03-30 18:50:42 -0500

    start menu page

---
+++ src/views/MenuPage.js
@@ -0,0 +1,26 @@
+import ScrollTransparentNavbar from "../components/Navbars/ScrollTransparentNavbar.js";
+
+import LandingPageHeader from "../components/Headers/LandingPageHeader.js";
+
+
+function MenuPage() {
+  React.useEffect(() => {
+    document.body.classList.add("landing-page");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("landing-page");
+    };
+  });
+  return (
+    <>
+      <ScrollTransparentNavbar />
+      <LandingPageHeader />
+      <div className="wrapper" type="primary">
+        <p>This is the Menu page</p>
+      </div>
+    </>
+  );
+}
+
+export default MenuPage;
