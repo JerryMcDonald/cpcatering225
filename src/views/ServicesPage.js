b8a34542b85a3377b41c26a7045ad38b8b70ff6b
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2021-03-30 18:50:50 -0500

    start services page

---
+++ src/views/ServicesPage.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+// reactstrap components
+import {
+  Container,
+  Row,
+  Col,
+} from "reactstrap";
+
+// Core Components
+import ScrollTransparentNavbar from "../components/Navbars/ScrollTransparentNavbar.js";
+
+import LandingPageHeader from "../components/Headers/LandingPageHeader.js";
+
+
+function ServicesPage() {
+  React.useEffect(() => {
+    document.body.classList.add("landing-page");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("landing-page");
+    };
+  });
+  return (
+    <>
+      <ScrollTransparentNavbar />
+      <LandingPageHeader />
+      <div className="wrapper" type="primary">
+        <p>This is the services page</p>
+      </div>
+    </>
+  );
+}
+
+export default ServicesPage;
