cc96cd578a21a51610241fca4be6ea84b8a922e2
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2021-03-30 21:33:48 -0500

    about component

---
+++ src/views/AboutPage.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+import ScrollTransparentNavbar from "../components/Navbars/ScrollTransparentNavbar.js";
+
+import LandingPageHeader from "../components/Headers/LandingPageHeader.js";
+
+
+function AboutPage() {
+  React.useEffect(() => {
+    document.body.classList.add("landing-page");
+    window.scrollTo(0, 0);
+    document.body.scrollTop = 0;
+    return function cleanup() {
+      document.body.classList.remove("landing-page");
+    };
+  });
+  return (
+    <>
+      <ScrollTransparentNavbar />
+      <LandingPageHeader />
+      <div className="wrapper" type="primary">
+        <p>This is the About page</p>
+      </div>
+    </>
+  );
+}
+
+export default AboutPage;
