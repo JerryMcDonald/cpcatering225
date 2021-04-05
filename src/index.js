16042d1a9ff6fcbbe3769b8356389471ca1c96bd
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2021-04-05 10:30:08 -0500

    add routes to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -11,12 +11,21 @@ import "../src/assets/demo/nucleo-icons-page-styles.css?v=1.4.0";
 
 import LandingPage from "../src/views/LandingPage.js";
 import AboutPage from"../src/views/AboutPage.js";
+import Services from "../src/views/ServicesPage.js";
+import MenuPage from "../src/views/MenuPage.js";
+import ContactPage from "../src/views/ContactPage.js";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" exact render={(props) => <LandingPage {...props} />} />
+      <Route path="/services" exact render={(props) => <Services {...props} />} />
+      <Route path="/menu" exact render={(props) => <MenuPage {...props} />} />
       <Route path="/about" exact render={(props) => <AboutPage {...props} />} />
+      <Route path="/contact" exact render={(props) => <ContactPage {...props} />} />
+
+
+
       <Redirect to="/" />
     </Switch>
   </BrowserRouter>,
