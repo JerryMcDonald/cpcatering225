379b720760c575ff6d952a27487cb9ecc50dd783
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2021-04-05 12:02:45 -0500

    new images to landing page

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,17 +1,12 @@
 import React from "react";
 
 // reactstrap components
-import {
-  Container,
-  Row,
-  Col,
-} from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 
 // Core Components
 import ScrollTransparentNavbar from "../components/Navbars/ScrollTransparentNavbar.js";
 
 import LandingPageHeader from "../components/Headers/LandingPageHeader.js";
-
 
 function LandingPage() {
   React.useEffect(() => {
@@ -27,65 +22,63 @@ function LandingPage() {
       <ScrollTransparentNavbar />
       <LandingPageHeader />
       <div className="section section-about-us">
-          <Container>
+        <Container>
+          <Row>
+            <Col className="ml-auto mr-auto text-center" md="8">
+              <h2 className="title">Christina and Paul Catering Services</h2>
+              <h5 className="description">
+                <p>This is where we can type info about cpCatering</p>
+              </h5>
+            </Col>
+          </Row>
+          <div className="separator separator-info"></div>
+          <div className="section-story-overview">
             <Row>
-              <Col className="ml-auto mr-auto text-center" md="8">
-                <h2 className="title">Who we are?</h2>
-                <h5 className="description">
-                  
-                </h5>
+              <Col md="6">
+                <div
+                  className="image-container image-left"
+                  style={{
+                    backgroundImage:
+                      "url(" +
+                      require("assets/img/cpcImages/cpCatering13.jpg") +
+                      ")",
+                  }}
+                >
+                  <p className="blockquote blockquote-info">
+                    <br></br>
+                    <br></br>
+                    <small>-CPCatering - cool little blue box</small>
+                  </p>
+                </div>
+                <div
+                  className="image-container image-left-bottom"
+                  style={{
+                    backgroundImage:
+                      "url(" +
+                      require("assets/img/cpcImages/cpCatering1.jpg") +
+                      ")",
+                  }}
+                ></div>
+              </Col>
+              <Col md="5">
+                <div
+                  className="image-container image-right"
+                  style={{
+                    backgroundImage:
+                      "url(" +
+                      require("assets/img/cpcImages/cpCatering11.jpeg") +
+                      ")",
+                  }}
+                ></div>
+                <h3></h3>
+                <p></p>
+                <p></p>
+                <p></p>
               </Col>
             </Row>
-            <div className="separator separator-info"></div>
-            <div className="section-story-overview">
-              <Row>
-                <Col md="6">
-                  <div
-                    className="image-container image-left"
-                    style={{
-                      backgroundImage:
-                        "url(" + require("assets/img/stock/stockCateringImage.jpeg") + ")",
-                    }}
-                  >
-                    <p className="blockquote blockquote-info">
-                    <br></br>
-                      <br></br>
-                      <small>-CPCatering</small>
-                    </p>
-                  </div>
-                  <div
-                    className="image-container image-left-bottom"
-                    style={{
-                      backgroundImage:
-                        "url(" + require("assets/img/stock/stockCateringImage.jpeg") + ")",
-                    }}
-                  ></div>
-                </Col>
-                <Col md="5">
-                  <div
-                    className="image-container image-right"
-                    style={{
-                      backgroundImage:
-                        "url(" + require("assets/img/stock/stockCateringImage.jpeg") + ")",
-                    }}
-                  ></div>
-                  <h3>
-                
-                  </h3>
-                  <p>
-     
-                  </p>
-                  <p>
-
-                  </p>
-                  <p>
-
-                  </p>
-                </Col>
-              </Row>
-            </div>
-          </Container>
-        </div>
+          </div>
+        </Container>
+      </div>
       <div className="wrapper" type="primary">
         <p>Hello</p>
       </div>
