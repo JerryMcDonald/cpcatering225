37f99000b9298313ad68d22a7edf57ca631c529c
JerryMcDonald <jmcdonald.dad.dev@gmail.com>
2021-04-12 09:20:17 -0500

    begin contact page testing

--- src/views/ContactPage.js
+++ src/views/ContactPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import emailjs from 'emailjs-com';
 
 import ScrollTransparentNavbar from "../components/Navbars/ScrollTransparentNavbar.js";
 
@@ -6,6 +7,7 @@ import LandingPageHeader from "../components/Headers/LandingPageHeader.js";
 
 
 function ContactPage() {
+
   React.useEffect(() => {
     document.body.classList.add("landing-page");
     window.scrollTo(0, 0);
@@ -14,6 +16,20 @@ function ContactPage() {
       document.body.classList.remove("landing-page");
     };
   });
+
+  function sendEmail(e) {
+    e.preventDefault();
+    
+    emailjs.sendForm('service_3jfn8sr', 'template_l3zulva', e.target, 'user_8DigZCEjUFfVEnYnS2Upl')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+
+  }
+
+
   return (
     <>
       <ScrollTransparentNavbar />
@@ -27,6 +43,16 @@ function ContactPage() {
       <br/>
       <div className="wrapper" type="primary">
         <p>This is the Contact page</p>
+            <form className="contact-form" onSubmit={sendEmail}>
+      <input type="hidden" name="contact_number" />
+      <label>Name</label>
+      <input type="text" name="user_name" />
+      <label>Email</label>
+      <input type="email" name="user_email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
       </div>
     </>
   );
